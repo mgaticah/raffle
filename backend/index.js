87a61b17551e1d29e00a54f1dfa64df23d383c4b
magh <manuel.gatica.holtmann@gmail.com>
2019-05-21 17:01:45 -0400

    incluye el uso de express.json()

--- backend/index.js
+++ backend/index.js
@@ -7,6 +7,7 @@ app.set('port', process.env.PORT || 3000);
 
 //middlewares
 app.use(morgan('dev'));
+app.use(express.json());
 //startup
 app.listen(app.get('port'), () => {
     console.log("servidor backend en ejecución. Puerto: "+app.get('port'));
